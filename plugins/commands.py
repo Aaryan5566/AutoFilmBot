bda08b7596839eb06d006602e6eeea246e993837
Aaryan5566 <arihansharma5467@gmail.com>
2024-10-29 18:07:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -30,8 +30,8 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('⚡ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ⚡', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('⚡ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ⚡', url="https://t.me/flashrequestmoviegroup"),
-                    InlineKeyboardButton('⚡ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ⚡', url='https://t.me/flashrequestmoviegroup')
+                    InlineKeyboardButton('⚡ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ⚡', url="https://t.me/flashmovie_s"),
+                    InlineKeyboardButton('⚡ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ⚡', url='https://t.me/flashmovie_s')
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
@@ -49,7 +49,7 @@ async def start(client, message):
                     InlineKeyboardButton('⚡ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ⚡', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
                     InlineKeyboardButton(' ⚡ᴇᴀʀɴ ᴍᴏɴᴇʏ⚡', callback_data="shortlink_info"),
-                    InlineKeyboardButton('⚡ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ⚡', url='https://t.me/flashmovie_s')
+                    InlineKeyboardButton('⚡ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ⚡', url='https://t.me/flashrequestmoviegroup')
                 ],[
                     InlineKeyboardButton('⚡ʜᴇʟᴘ⚡', callback_data='help'),
                     InlineKeyboardButton('⚡ᴀʙᴏᴜᴛ⚡', callback_data='about')

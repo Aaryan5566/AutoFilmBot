1f1f5ca39f4a5316cc4372ee390cee5bf1d1e953
Aaryan5566 <arihansharma5467@gmail.com>
2024-10-30 11:08:36 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -2,15 +2,15 @@
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-from pyrogram import Client, filters, enums
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
+from pyrofork import Client, filters, enums
+from pyrofork.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrofork.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
 from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT, MELCOW_NEW_USERS, MELCOW_VID, CHNL_LNK, GRP_LNK
 from database.users_chats_db import db
 from database.ia_filterdb import Media
 from utils import get_size, temp, get_settings
 from Script import script
-from pyrogram.errors import ChatAdminRequired
+from pyrofork.errors import ChatAdminRequired
 import asyncio 
 
 """-----------------------------------------https://t.me/GetTGLink/4179 --------------------------------------"""

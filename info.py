28f1e2cdddad65cce7824ae5d612eeba492c543b
Aaryan5566 <arihansharma5467@gmail.com>
2024-11-10 14:07:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,7 +16,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '23378704'))
 API_HASH = environ.get('API_HASH', '15a02b4d02babeb79e8f328b0ead0c17')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7371099584:AAGXeRDvQIAxOJ3Oi5rjpDiNQE8hUxcn9CE")
+BOT_TOKEN = environ.get('BOT_TOKEN', ""7925151038:AAHBbt8BApY7jrUW1hWZSXYzSwZtcCk5WPg")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -29,7 +29,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7364818327').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002427767556').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002373762249').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '6302537270').split()]
@@ -62,7 +62,7 @@ CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/flashmovie_s')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/flashmovie_s')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'Hello Nanbha and Nanbis ❤️')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002427767556'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002373762249'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/flashrequestmoviegroup')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
@@ -99,8 +99,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "".format(FQDN, PORT)
+URL = "https://mixed-laurianne-flashpoint-f3f0ec98.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://mixed-laurianne-flashpoint-f3f0ec98.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'MYD-BOY'))
@@ -115,9 +115,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "".format(FQDN)
+    URL = "https://mixed-laurianne-flashpoint-f3f0ec98.koyeb.app/".format(FQDN)
 else:
-    URL = "".format(FQDN)
+    URL =  "https://mixed-laurianne-flashpoint-f3f0ec98.koyeb.app/".format(FQDN)
 
 
 # Replace '@yourchannelname' with your channel's username

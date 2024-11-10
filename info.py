7b2b917e719c6adcfa4455d80efb22e40575b5f4
Aaryan5566 <arihansharma5467@gmail.com>
2024-11-10 13:44:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -99,8 +99,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://mixed-laurianne-flashpoint-f3f0ec98.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://mixed-laurianne-flashpoint-f3f0ec98.koyeb.app/".format(FQDN, PORT)
+URL = "".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'MYD-BOY'))
@@ -115,9 +115,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://mixed-laurianne-flashpoint-f3f0ec98.koyeb.app/".format(FQDN)
+    URL = "".format(FQDN)
 else:
-    URL = "https://mixed-laurianne-flashpoint-f3f0ec98.koyeb.app/".format(FQDN)
+    URL = "".format(FQDN)
 
 
 # Replace '@yourchannelname' with your channel's username

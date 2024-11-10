501006aaf552826d574d6d440c1ad27621a2f5a1
Aaryan5566 <arihansharma5467@gmail.com>
2024-11-10 13:39:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -99,7 +99,7 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://mixed-laurianne-flashpoint-f3f0ec98.koyeb.app".format(FQDN) if ON_HEROKU or NO_PORT else \
+URL = "https://mixed-laurianne-flashpoint-f3f0ec98.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "https://mixed-laurianne-flashpoint-f3f0ec98.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
